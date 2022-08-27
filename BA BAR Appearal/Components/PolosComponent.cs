13698dd0abb2d8182e5a7db487374de9936ef16e
Umer-W <Bsef19a025@pucit.edu.pk>
2022-08-27 03:00:25 -0700

    changing in trouser page

--- BA BAR Appearal/Components/PolosComponent.cs
+++ BA BAR Appearal/Components/PolosComponent.cs
@@ -75,12 +75,7 @@ namespace BA_BAR_Appearal.Components
                 path = "/lib/Polos_Shirt11.jpg",
                 price = 1049.00
             });
-            polo.Add(new Polos
-            {
-                name = "Maroon Round Neck T-shirt",
-                path = "/lib/Polos_Shirt12.jpg",
-                price = 1049.00
-            });
+         
             return View(polo);
         }
     }
