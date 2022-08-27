dbdf608224c83692c55d6aca29238ded5b27f24c
Umer-W <Bsef19a025@pucit.edu.pk>
2022-08-27 02:53:27 -0700

    view component

---
+++ BA BAR Appearal/Components/PolosComponent.cs
@@ -0,0 +1,69 @@
+﻿using BA_BAR_Appearal.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BA_BAR_Appearal.Components
+{
+    [ViewComponent(Name="PolosComponent")]
+    public class PolosComponent:ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            List<Polos> polo = new List<Polos>();
+            polo.Add(new Polos{
+                name = "Maroon Round Neck T-shirt",
+                path = "/lib/Polos_Shirt1.jpg",
+                price = 1049.00
+            });
+            polo.Add(new Polos
+            {
+                name = "Maroon Round Neck T-shirt",
+                path = "/lib/Polos_Shirt2.jpg",
+                price = 1049.00
+            });
+            polo.Add(new Polos
+            {
+                name = "Maroon Round Neck T-shirt",
+                path = "/lib/Polos_Shirt3.jpg",
+                price = 1049.00
+            });
+            polo.Add(new Polos
+            {
+                name = "Maroon Round Neck T-shirt",
+                path = "/lib/Polos_Shirt4.jpg",
+                price = 1049.00
+            });
+            polo.Add(new Polos
+            {
+                name = "Maroon Round Neck T-shirt",
+                path = "/lib/Polos_Shirt5.jpg",
+                price = 1049.00
+            });
+            polo.Add(new Polos
+            {
+                name = "Maroon Round Neck T-shirt",
+                path = "/lib/Polos_Shirt6.jpg",
+                price = 1049.00
+            });
+            polo.Add(new Polos
+            {
+                name = "Maroon Round Neck T-shirt",
+                path = "/lib/Polos_Shirt7.jpg",
+                price = 1049.00
+
+            });
+            polo.Add(new Polos
+            {
+                name = "Maroon Round Neck T-shirt",
+                path = "/lib/Polos_Shirt8.jpg",
+                price = 1049.00
+            });
+            polo.Add(new Polos
+            {
+                name = "Maroon Round Neck T-shirt",
+                path = "/lib/Polos_Shirt9.jpg",
+                price = 1049.00
+            });
+            return View(polo);
+        }
+    }
+}
---
+++ BA BAR Appearal/Models/Polos.cs
@@ -0,0 +1,9 @@
+﻿namespace BA_BAR_Appearal.Models
+{
+    public class Polos
+    {
+        public string path { get; set; }
+        public string name { get; set; }
+        public double price { get; set; }
+    }
+}
